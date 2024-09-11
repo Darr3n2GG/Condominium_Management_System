12e4703c402187fd29945240561dd50417e200b9
Darr3n <darrensoonyuchien@gmail.com>
2024-09-11 12:18:02 +0800

    added elements for profile and home page

--- profile.php
+++ profile.php
@@ -35,7 +35,11 @@ $stmt->close();
 	<body class="loggedin">
 		<nav class="navtop">
 			<div>
-				<h1>Website Title</h1>
+                <h1>Website Title</h1>
+                <a href="test.php"><i class="fas fa-user-circle"></i>About</a>
+                <a href="test.php"><i class="fas fa-user-circle"></i>Issues</a>
+                <a href="test.php"><i class="fas fa-user-circle"></i>Rent Payment</a>
+                <a href="home.php"><i class="fa-solid fa-house"></i>Home</a>
 				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
 				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
 			</div>
