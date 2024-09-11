ab44de9361cf189b992d946791081a498de0ddf3
Bryan-CoderS <139633909+Bryan-CoderS@users.noreply.github.com>
2024-09-11 12:20:52 +0800

    Address & Website title
    + address for registration
    + changed web title

--- profile.php
+++ profile.php
@@ -28,14 +28,14 @@ $stmt->close();
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Profile Page</title>
+		<title>World Residence Centre</title>
 		<link href="style.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
 	</head>
 	<body class="loggedin">
 		<nav class="navtop">
 			<div>
-				<h1>Website Title</h1>
+				<h1>World Residence Centre</h1>
 				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
 				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
 			</div>
@@ -53,6 +53,9 @@ $stmt->close();
 					<tr>
 						<td>Email:</td>
 						<td><?=htmlspecialchars($email, ENT_QUOTES)?></td>
+					</tr>
+					<tr>
+						<td>House Address:</td>
 					</tr>
 				</table>
 			</div>
