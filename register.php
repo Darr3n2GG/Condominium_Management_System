846e420f385a7c794d393daed5475d822c2c9af0
Darr3n <darrensoonyuchien@gmail.com>
2024-11-16 12:50:10 +0800

    added house number in register and profile
    #IMPORTANT
    I need to know if the house number should be entered in register or given if a certain condition is met (eg. the condo manager selected a house number for them).

--- register.php
+++ register.php
@@ -1,24 +1,18 @@
 <?php
-// Change this to your connection info.
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
 $DATABASE_NAME = 'exampledb';
-// Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
-	// If there is an error with the connection, stop the script and display the error.
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
-// Now we check if the data was submitted, isset() function will check if the data exists.
-if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
-	// Could not get the data that should have been sent.
+if (!isset($_POST['username'], $_POST['password'], $_POST['email'], $_POST['house_number'])) {
 	exit('Please complete the registration form!');
 }
-// Make sure the submitted registration values are not empty.
-if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
-	// One or more values are empty.
+
+if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email']) || empty($_POST['house_number'])) {
 	exit('Please complete the registration form');
 }
 
@@ -34,33 +28,57 @@ if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
 	exit('Password must be between 5 and 20 characters long!');
 }
 
-// We need to check if the account with that username exists.
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-	// Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
-	$stmt->bind_param('s', $_POST['username']);
-	$stmt->execute();
-	$stmt->store_result();
-	// Store the result so we can check if the account exists in the database.
-	if ($stmt->num_rows > 0) {
-		// Username already exists
-		echo 'Username exists, please choose another!';
-	} else {
-		// Username doesn't exists, insert new account
-        if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)')) {
-            // We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
-            $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-            $stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
-            $stmt->execute();
-            echo nl2br('You have successfully registered! You can now login! Enter http://localhost/System_Management_Web/login.html.');
-        } else {
-            // Something is wrong with the SQL statement, so you must check to make sure your accounts table exists with all three fields.
-            echo 'Could not prepare statement!';
-        }
-	}
-	$stmt->close();
-} else {
-	// Something is wrong with the SQL statement, so you must check to make sure your accounts table exists with all 3 fields.
-	echo 'Could not prepare statement!';
+function bind_parameters($stmt, $types, ...$params) {
+    $stmt->bind_param($types, ...$params);
+    $stmt->execute();
+    $stmt->store_result();
+    return $stmt;
 }
+function check_username_exists() {
+    $stmt = $GLOBALS['con']->prepare('SELECT id FROM accounts WHERE username = ?');
+    $stmt = bind_parameters($stmt, 's', $_POST['username']);
+    $exists = $stmt->num_rows > 0;
+    $stmt->close();
+    return $exists;
+}
+function check_house_number_exists() {
+    $stmt = $GLOBALS['con']->prepare('SELECT id FROM accounts WHERE house_number = ?');
+    $stmt = bind_parameters($stmt, 's', $_POST['house_number']);
+    $exists = $stmt->num_rows > 0;
+    $stmt->close();
+    return $exists;
+}
+function insert_new_account() {
+    $con = $GLOBALS['con'];
+    if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email, house_number) VALUES (?, ?, ?, ?)')) {
+        // We do not want to expose passwords in our database
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $stmt->bind_param('ssss', $_POST['username'], $password, $_POST['email'], $_POST['house_number']);
+        $stmt->execute();
+        echo nl2br('You have successfully registered! You can now login! Enter http://localhost/System_Management_Web/login.html.');
+    } else {
+        error_in_prepare_statement();
+    }
+}
+function error_in_prepare_statement() {
+    // Something is wrong with the SQL statement, check to make sure your accounts table exists with all three fields.
+    echo 'Could not prepare statement!';
+}
+function check_account_exists() {
+    if (check_username_exists()) {
+        echo 'Username exists, please choose another!';
+        return false;
+    } elseif (check_house_number_exists()) {
+        echo 'House Number exists, please choose another!';
+        return false;
+    } else {
+        return true;
+    }
+}
+
+if (check_account_exists()) {
+    insert_new_account();
+}
+
 $con->close();
 ?>
