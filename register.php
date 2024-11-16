d1414960200f35183f3090840a28954d22f81d80
Darr3n <darrensoonyuchien@gmail.com>
2024-11-16 13:33:13 +0800

    tweaked authenticate.php and register.php

--- register.php
+++ register.php
@@ -43,7 +43,7 @@ if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
 	</script>";
 }
 
-function bind_parameters($stmt, $types, ...$params) {
+function execute_and_store($stmt, $types, ...$params) {
     $stmt->bind_param($types, ...$params);
     $stmt->execute();
     $stmt->store_result();
@@ -51,14 +51,14 @@ function bind_parameters($stmt, $types, ...$params) {
 }
 function check_username_exists() {
     $stmt = $GLOBALS['con']->prepare('SELECT id FROM accounts WHERE username = ?');
-    $stmt = bind_parameters($stmt, 's', $_POST['username']);
+    $stmt = execute_and_store($stmt, 's', $_POST['username']);
     $exists = $stmt->num_rows > 0;
     $stmt->close();
     return $exists;
 }
 function check_house_number_exists() {
     $stmt = $GLOBALS['con']->prepare('SELECT id FROM accounts WHERE house_number = ?');
-    $stmt = bind_parameters($stmt, 's', $_POST['house_number']);
+    $stmt = execute_and_store($stmt, 's', $_POST['house_number']);
     $exists = $stmt->num_rows > 0;
     $stmt->close();
     return $exists;
@@ -68,9 +68,7 @@ function insert_new_account() {
     if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email, house_number) VALUES (?, ?, ?, ?)')) {
         // We do not want to expose passwords in our database
         $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-        $stmt->bind_param('ssss', $_POST['username'], $password, $_POST['email'], $_POST['house_number']);
-        $stmt->execute();
-        $stmt->close();
+        $stmt = execute_and_store($stmt, 'ssss', $_POST['username'], $password, $_POST['email'], $_POST['house_number']);
         echo "<script type='text/javascript'>
 				alert('Log in success! Redirecting to log in page...');
 				window.location.href = 'login.html';
