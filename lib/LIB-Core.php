4889017a5fb6a2674ea4767e43d32059dafec2ce
Darr3n <darrensoonyuchien@gmail.com>
2024-11-21 22:58:33 +0800

    tweaked code of core module

--- lib/LIB-Core.php
+++ lib/LIB-Core.php
@@ -8,7 +8,7 @@ class Core {
     public function __construct() {
         $this->con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 
-        if ( mysqli_connect_errno() ) {
+        if (mysqli_connect_errno()) {
             exit("Failed to connect to MySQL: " . mysqli_connect_error());
         }
     }
