9387f587cb8574ca94248cd1024e90fef37ff8ce
Darr3n <darrensoonyuchien@gmail.com>
2024-12-01 09:57:26 +0800

    debug

--- backend/logout.php
+++ backend/logout.php
@@ -1,9 +1,4 @@
 <?php
 session_start();
 session_destroy();
-<<<<<<<< HEAD:backend/logout.php
 header("Location: ../frontend/login.html");
-?>
-========
-header("Location: login.html");
->>>>>>>> main:logout.php
