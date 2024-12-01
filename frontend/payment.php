8423331101fe42f5518ac854dcb641e2fd27eb2d
Darr3n <darrensoonyuchien@gmail.com>
2024-12-01 09:35:06 +0800

    debug

--- frontend/payment.php
+++ frontend/payment.php
@@ -34,8 +34,8 @@ if ($payment_row) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Island Crest - Payment</title>
-    <link href="../assets/style.css" rel="stylesheet" type="text/css">
-    <link href="../assets/payment_feedback.css" rel="stylesheet" type="text/css">
+    <link href="../assets/home.css" rel="stylesheet" type="text/css">
+    <link href="../assets/payment.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
 </head>
 
